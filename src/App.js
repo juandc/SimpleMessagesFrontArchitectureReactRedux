3c7802152630e093a01421b67acfe75e20a51b0e
Juan David Castro <me@juandc.co>
2019-10-04 13:17:38 -0500

    Simple Development Setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <h1>¡Hola!</h1>,
+  document.getElementById('app'),
+);
+
+module.hot.accept();
